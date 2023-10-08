e1025a8d229d6e2bdba843941d21946ee4788706
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-10-08 22:15:43 +0530

    Update cellPage.tsx

--- src/Components/cellPage.tsx
+++ src/Components/cellPage.tsx
@@ -1,25 +1,3 @@
-// import React from "react";
-
-// interface CellProps {
-//   isFood: boolean;
-//   isSnake: boolean;
-//   isSnakeHead: boolean;
-// }
-
-// const Cell: React.FC<CellProps> = ({ isFood, isSnake, isSnakeHead }) => {
-//   const classes = `
-//     w-30 h-30
-//     ${isFood ? "bg-red-500" : ""}
-//     ${isSnake ? "bg-green-500" : ""}
-//     ${isSnakeHead ? "rounded-full overflow-hidden bg-[black]" : "bg-white"}
-//   `;
-
-//   return <div className={classes}></div>;
-// };
-
-// export default Cell;
-
-// components/Cell.tsx
 import React from "react";
 
 interface CellProps {
